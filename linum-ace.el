95943eeea80600c1a5cc6bd65abc3b8b8b9e51b1
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-05-30 23:38:44 +0800

    chnage version to 0.3

--- linum-ace.el
+++ linum-ace.el
@@ -5,7 +5,7 @@
 ;; Author: coldnew <coldnew.tw@gmail.com>
 ;; Keywords: converience
 ;; X-URL: http://www.emacswiki.org/cgi-bin/wiki/download/linum-ace.el
-(defconst linum-ace-version "0.2")
+(defconst linum-ace-version "0.3")
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
